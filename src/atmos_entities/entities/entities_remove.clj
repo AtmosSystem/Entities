ba21e2679eff23993b493b9fad98f9937bbe84d4
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-02-16 00:44:29 -0400

    Adding functionality

--- src/atmos_entities/entities/entities_remove.clj
+++ src/atmos_entities/entities/entities_remove.clj
@@ -1,15 +1,28 @@
 (in-ns 'atmos-entities.core)
 
+(def ^:private remove-persist-entity-base* (-> (delete* entities)))
+
+(defn- remove-persist-entity*
+  [id]
+  (-> remove-persist-entity-base*
+      (where {:id id})
+      delete))
+
 (defn- remove-persist-entities*
-  [where-filter]
-  (delete entities
-          (where where-filter)))
+  [ids]
+  (-> remove-persist-entity-base*
+      (where {:id [in ids]})
+      delete))
 
 (defn- remove-entities*
-  [id]
-  (let [where-filter {:id id}]
-    (if-let [entity (get-entity id)]
-      (do
-        (remove-persist-entities* where-filter)
-        true)
-      false)))
+  [data]
+  (let [remove-fn (fn
+                    [remove-entity-fn]
+                    (if-let [entity (get-entities* data)]
+                      (do
+                        (remove-entity-fn data)
+                        true)
+                      false))]
+    (cond
+      (number? data) (remove-fn remove-persist-entity*)
+      (vector? data) (remove-fn remove-persist-entities*))))
--- src/atmos_entities/implementation.clj
+++ src/atmos_entities/implementation.clj
@@ -4,7 +4,10 @@
 ; BEGIN General functions
 ;------------------------------
 
-(defn defpersistence [db-map] (sql/mysql db-map))
+(defn defpersistence [db-map]
+  (let [db-map (clojure.core/update db-map
+                                    :classname #(str "com.mysql.cj.jdbc.Driver" %))]
+    (sql/mysql db-map)))
 
 (defn init-persistence [db-definition] (sql/defdb atmos-entities db-definition))
 
