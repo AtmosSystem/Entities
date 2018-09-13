af68ed77993dc8dfb24016631745ffb0b6ac75ee
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-09-13 17:50:34 -0400

    Refactoring

--- src/atmos_entities/implementation/contacts/contacts_update.clj
+++
@@ -1,6 +0,0 @@
-(in-ns 'atmos-entities.implementation.core)
-
-
-(declare update-contacts*)
-
-(defupdate-entity contacts :id)
--- src/atmos_entities/implementation/entities/entities_add.clj
+++
@@ -1,5 +0,0 @@
-(in-ns 'atmos-entities.implementation.core)
-
-(declare add-entities*)
-
-(defadd-entity entities)
